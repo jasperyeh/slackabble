a2af7fe0ed01f134b1beb261dc78f9afb30bc791
Jasper Yeh <jasper@JASPER-MBP.local>
2019-04-11 23:21:34 -0700

    Copy to clipboard button works, creating/removing button needs to be redone

--- main.js
+++ main.js
@@ -12,15 +12,43 @@ function scrabbler() {
 		location.reload();
 	}
 
+	// Translate chars into Scrabble emoji text
 	for (var i = 0; i < chars.length; i++) {
 		output += `:scrabble-${chars[i]}:`;
 	}
 	// Display output to HTML
 	document.getElementById('output').innerHTML = output;
+
 	// Clear textbox
 	document.getElementById('input').value = '';
+
+	var button = document.createElement('button');
+	button.innerHTML = 'Copy to clipboard';
+	// where do we want to have the button to appear?
+	// you can append it to another element just by doing something like
+	// document.getElementById('foobutton').appendChild(button);
+	document.body.appendChild(button);
+
+	button.onclick = function() {
+		copy(output);
+		document.body.removeChild(button);
+	};
 }
 
+function copy(output) {
+	// Copy text to clipboard
+	// https://stackoverflow.com/a/46118025
+	var temp = document.createElement('input');
+	document.body.appendChild(temp);
+	temp.setAttribute('value', output);
+	temp.select();
+	document.execCommand('copy');
+	document.body.removeChild(temp);
+
+	/* Alert the copied text */
+	alert('Copied to clipboard!');
+	// document.getElementById('button').body.removeChild(button);
+}
 // Press enter to click button
 // https://stackoverflow.com/a/155263
 document.getElementById('input').addEventListener('keyup', function(event) {
